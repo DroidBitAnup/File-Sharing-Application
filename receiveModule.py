69caf34ac130e21f84ab640e3c9e4e21d94b84e4
DroidBitAnup <codeanup2503@gmail.com>
2025-10-18 01:51:51 +0530

    Save local changes

--- receiveModule.py
+++ receiveModule.py
@@ -47,13 +47,14 @@ def Receive():
             if running:
                 return
             port = 5001
-            ip = ListingIP_var.get().strip()
+            ip = "0.0.0.0"
             running = True
             start.config(state="disabled")
             Close.config(state="normal")
-            status_label.config(text=f"Starting server on {ip}:{port}...")
             accept_thread = threading.Thread(target=run_server, args=(ip, port), daemon=True)
             accept_thread.start()
+            
+            status_label.config(text="Ready to receive...")
         except Exception as es:
             messagebox.showerror("Error",f"Due To:{str(es)}",parent=main)
 
@@ -74,12 +75,20 @@ def Receive():
 
     def run_server( ip, port):
             global running
+            progress["value"] =0
+            percentage_label.config(text="")
+            speed_label_Numeric.config(text="")
+            ETA_label_Numeric.config(text="") 
+            slash.config(text="")
+            ReceivedSize_label.config(text="")
+            totalSize_label.config(text="")
             try:
                 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                 server_socket.bind((ip, port))
                 server_socket.listen(5)
-                status_label.config(text=f"Listening on {ip}:{port}")
+                time.sleep(2)
+                status_label.config(text="Waiting for sender...")
             except Exception as e:
                 status_label.config(text="Failed to start server")
                 messagebox.showerror("Error", str(e),parent=main)
@@ -107,8 +116,11 @@ def Receive():
                 pass
 
     def handle_client( conn, addr):
+        sizetxt=""
+        totalfilesize=0.0
+        receivedsize=0.0
         try:
-            status_label.config(text=f"Connection from {addr[0]}:{addr[1]}")
+            #status_label.config(text=f"Connection from {addr[0]}")
             # receive header (filename and filesize)
             header_bytes = conn.recv(BUFFER_SIZE)
             if not header_bytes:
@@ -119,11 +131,25 @@ def Receive():
                 # malformed header
                 conn.close()
                 return
-            filename, filesize_str = header.split(SEPARATOR)
+            filename, filesize_str,sender= header.split(SEPARATOR)
             filesize = int(filesize_str)
+            status_label.config(text=f"Connection from {sender}")
 
             safe_filename = os.path.basename(filename)
             save_path = os.path.join(save_folder_var.get(), safe_filename)
+
+            #Fillesize convertion
+            if filesize>=(1024*1024) and filesize<(1024*1024*1024):
+                totalfilesize=filesize/(1024*1024)
+                sizetxt="MB"
+            elif filesize>=(1024*1024*1024): 
+                totalfilesize=filesize/(1024*1024*1024)
+                sizetxt="GB"
+            else:
+                totalfilesize=filesize/(1024)
+                sizetxt="KB"
+
+
 
             # if file exists, create a new name
             base, ext = os.path.splitext(save_path)
@@ -136,9 +162,18 @@ def Receive():
             progress["value"] = 0
             received = 0
             time.sleep(5)
-            status_label.config(text=f"Receiving {safe_filename} ({filesize} bytes) ...")
+            slash.config(text="/")
+            totalSize_label.config(text=f"{totalfilesize:.2f} {sizetxt}")
+            status_label.config(text=f"Receiving {safe_filename}")
 
             with open(save_path, "wb") as f:
+                
+                start = time.time()
+                last_time = start
+                last_recv = 0
+                percent=0
+                speed=0
+                eta=0
                 while received < filesize:
                     chunk = conn.recv(BUFFER_SIZE)
                     if not chunk:
@@ -146,75 +181,139 @@ def Receive():
                     f.write(chunk)
                     received += len(chunk)
                     progress["value"] = received
+
+                    #Recieved size convertion
+                    if sizetxt=="KB":
+                        receivedsize=received/1024
+
+                    elif sizetxt=="MB":
+                        receivedsize=received/(1024*1024)    
+
+                    elif sizetxt=="GB":
+                        receivedsize=received/(1024*1024*1024)
+
+
+                    now = time.time()
+                    if now - last_time >= 1:
+                        percent = (received / filesize) * 100
+                        diff = received - last_recv
+                        speed = diff / (now - last_time) / (1024 * 1024)
+                        eta = (filesize - received) / (speed * 1024 * 1024) if speed > 0 else 0
+
+                        last_time = now
+                        last_recv = received
+                        percentage_label.config(text=f"{percent:.2f}%")
+                        speed_label_Numeric.config(text=f"{speed:.2f} MB/s") 
+                        ETA_label_Numeric.config(text=f"{eta/60:.2f} Minutes") 
+                        ReceivedSize_label.config(text=f"{receivedsize:.2f} {sizetxt}")
                     main.update_idletasks()
+
+                    
 
             conn.close()
             if received >= filesize:
                 status_label.config(text=f"Received: {safe_filename} -> {save_path}")
+                progress["value"] = filesize
+                percentage_label.config(text="100%")
+                speed_label_Numeric.config(text="")
+                ETA_label_Numeric.config(text="")
                 messagebox.showinfo("Received", f"Saved to: {save_path}",parent=main)
             else:
                 status_label.config(text="Receive incomplete / connection closed")
+                percentage_label.config(text=f"{percent:.2f}%")
+                speed_label_Numeric.config(text="")
+                ETA_label_Numeric.config(text="")
+                progress["value"] = received
                 messagebox.showwarning("Warning", "File transfer incomplete.",parent=main)
-            progress["value"] = 0
         except Exception as e:
             try:
                 conn.close()
             except:
                 pass
             status_label.config(text="Error receiving file")
+            progress["value"] = received
+            percentage_label.config(text=f"{percent:.2f}%")
+            speed_label_Numeric.config(text="")
+            ETA_label_Numeric.config(text="")  
             messagebox.showerror("Error", str(e),parent=main)    
 
     #Icon
     image_icon=PhotoImage(file=r"Images\receive.png")
     main.iconphoto(True,image_icon)
 
-    #Icon
-    image_icon=PhotoImage(file=r"Images\send.png")
-    main.iconphoto(True,image_icon)
+    ##Icon
+    #image_icon=PhotoImage(file=r"Images\send.png")
+    #main.iconphoto(True,image_icon)
 
     sendbackground=PhotoImage(file=r"Images/sender.png")
-    sendback=Label(main,image=sendbackground)
+    sendback=Label(main,image=sendbackground,bg="#ab95b5")
     sendback.place(x=10,y=15)
 
     # Separator Line
-    Frame(main,width=500,height=2,bg="#f3f5f6").place(x=0,y=220)
+    Frame(main,width=500,height=2,bg="#f3f5f6").place(x=0,y=225)
 
 
-    #Lower Frame
-    mainbackground=PhotoImage(file=r"Images/id.png")
-    back=Label(main,image=mainbackground,bg="#ab95b5")
-    back.place(x=100,y=320)
+    #singnal and ID
+    signalID=PhotoImage(file=r"Images/id.png")
+    back=Label(main,image=signalID,bg="#ab95b5")
+    back.place(x=100,y=325)
 
     # Get host name
-    ip_address=''
-    try:
-        host = socket.gethostname()
-        ip_address = socket.gethostbyname(host)
-        print(f"Your Local IP Address: {ip_address}")
-    except socket.gaierror as e:
-        print(e)
+    host = socket.gethostname()
     
-    Label(main,text=f'ID: {host}',fg='black').place(x=140,y=340)
-    Label(main,text=f'IP: {ip_address}',fg='black').place(x=140,y=360)
+    Label(main,text=f'ID: {host}',fg='black',bg="white").place(x=140,y=350)
 
     #browse_folder
-    Label(main,text="Save Folder:",font =('arial',10),bg="#ab95b5",fg="#070b12").place(x=160,y=60)
-    Label(main, textvariable=save_folder_var, width=33, anchor="w", relief="sunken").place(x=160,y=90)
+    Label(main,text="Save File Location:",font =('arial',13),bg="#ab95b5",fg="#070b12").place(x=160,y=50)
+    Label(main, textvariable=save_folder_var, width=33, anchor="w", relief="sunken").place(x=160,y=80)
 
-    #Enter Listing IP
-    Label(main,text="Enter Listing IP:",font =('arial',10),bg="#ab95b5",fg="#070b12").place(x=160,y=5)
-    Entry(main, textvariable=ListingIP_var, width=33).place(x=160,y=30)
 
     #Status Label
     status_label = Label(main, text="Status: Stopped",bg="#ab95b5")
-    status_label.place(x=15,y=240)
+    status_label.place(x=15,y=290)
+
+    
+
+    #Percentage
+    percentage_label=Label(main, text=" ",font =('arial',12),bg="#ab95b5")
+    percentage_label.place(x=400,y=185)
+
+    #Speed
+    speed_label=Label(main, text="Speed",font =('arial',14,'bold'),bg="#ab95b5")
+    speed_label.place(x=20,y=225)
+    speed_label_Numeric=Label(main, text="",font =('arial',12),bg="#ab95b5")
+    speed_label_Numeric.place(x=10,y=250)
+
+    #ETA
+    ETA_label=Label(main, text="ETA",font =('arial',14,'bold'),bg="#ab95b5")
+    ETA_label.place(x=362,y=225)
+    ETA_label_Numeric=Label(main, text="",font =('arial',12),bg="#ab95b5")
+    ETA_label_Numeric.place(x=336,y=250)
+
+    ##Filesize
+    #Filesize_label=Label(main, text="Filesize",font =('arial',14,'bold'),bg="#ab95b5")
+    #Filesize_label.place(x=195,y=225)
+    #Filesize_numeric=Label(main, text="100 MB",font =('arial',11),bg="#ab95b5")
+    #Filesize_numeric.place(x=176,y=250)
+
+    #Received slash Totalsize
+    slash=Label(main,text="",font =('arial',15,'bold'),bg="#ab95b5")
+    slash.place(x=185,y=198)
+    totalSize_label=Label(main,text="",font =('arial',11),bg="#ab95b5")
+    totalSize_label.place(x=195,y=201)
+
+    ReceivedSize_label=Label(main,text="",font =('arial',11),bg="#ab95b5")
+    ReceivedSize_label.place(x=105,y=201)
 
     #progress Bar
-    progress = Progressbar(main, length=420, mode="determinate")
-    progress.place(x=12,y=185)
+    progress = Progressbar(main, length=382, mode="determinate")
+    progress.place(x=12,y=178)
+
+    # Separator Line
+    Frame(main,width=500,height=2,bg="#f3f5f6").place(x=0,y=225)
 
     #buttons
-    start=Button(main,text="Start",command=start_server_thread,width=10,height=1,font='arial 14 bold',bg="#fff",fg="#000")
+    start=Button(main,text="Receive",command=start_server_thread,width=10,height=1,font='arial 14 bold',bg="#fff",fg="#000")
     start.place(x=15,y=130)
 
     Close=Button(main,text="Close",command=stop_server,width=10,height=1,font='arial 14 bold',bg="#fff",fg="#000")
--- sendModule.py
+++ sendModule.py
@@ -41,38 +41,61 @@ def Send():
     def sender():
         global filename
         try:
-            host=socket.gethostname()
-            host= IP_var.get().strip()
+            hostname= IP_var.get().strip()
             port=5001
             if not host or  not filename:
-                messagebox.showerror("Error", "Please enter receiver IP, port and select a file.",parent=window)
+                messagebox.showerror("Error", "Please enter receiver ID, and select a file.",parent=window)
                 return
-            t = threading.Thread(target=send_file, args=(host, port, filename), daemon=True)
+            t = threading.Thread(target=send_file, args=(port, filename,hostname), daemon=True)
             t.start()
             Send_Btn.config(state="disabled")
         except Exception as es:
                 messagebox.showerror("Error",f"Due To:{str(es)}",parent=window)     
     
-    def send_file(host, port, filepath):    
+    def send_file( port, filepath,hostname):
+        sizetxt=""
+        totalfilesize=0.0
+        sentsize=0.0    
+        sent = 0
+        start = time.time()
+        last_time = start
+        last_sent = 0
+        percent=0
         try:
             filesize = os.path.getsize(filepath)
             filename = os.path.basename(filepath)
     
-            status_label.config(text=f"Connecting to {host}:{port}...")
+            status_label.config(text=f"Connecting to {hostname}")
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(10)
-            sock.connect((host, port))
+            sock.connect((hostname, port))
             sock.settimeout(None)
     
-            header = f"{filename}{SEPARATOR}{filesize}".encode()
+            header = f"{filename}{SEPARATOR}{filesize}{SEPARATOR}{hostname}".encode()
             sock.sendall(header)  # send header in one shot (small)
             # optional small delay could be used if receiver expects but not necessary
     
             sent = 0
             progress["maximum"] = filesize
             progress["value"] = 0
+            
+            
+            #Fillesize convertion
+            if filesize>=(1024*1024) and filesize<(1024*1024*1024):
+                totalfilesize=filesize/(1024*1024)
+                sizetxt="MB"
+            elif filesize>=(1024*1024*1024): 
+                totalfilesize=filesize/(1024*1024*1024)
+                sizetxt="GB"
+            else:
+                totalfilesize=filesize/(1024)
+                sizetxt="KB"
+
             time.sleep(5)
-            status_label.config(text=f"Sending {filename} ({filesize} bytes)...")
+            slash.config(text="/")
+            totalSize_label.config(text=f"{totalfilesize:.2f} {sizetxt}")
+            status_label.config(text=f"Sending {filename}")
+
     
             with open(filepath, "rb") as f:
                 while True:
@@ -81,19 +104,50 @@ def Send():
                         break
                     sock.sendall(bytes_read)
                     sent += len(bytes_read)
-                    # update progress bar
-                    progress["value"] = sent
-                    # force UI update
+                    now = time.time()
+                    if now - last_time >= 1:  # update every second
+                        percent = (sent / filesize) * 100
+                        diff = sent - last_sent
+                        speed = diff / (now - last_time) / (1024 * 1024)  # MB/s
+                        eta = (filesize - sent) / (speed * 1024 * 1024) if speed > 0 else 0
+
+                        last_time = now
+                        last_sent = sent
+                        # update progress bar
+
+                        #Recieved size convertion
+                        if sizetxt=="KB":
+                            sentsize=sent/1024
+
+                        elif sizetxt=="MB":
+                            sentsize=sent/(1024*1024)    
+
+                        elif sizetxt=="GB":
+                            sentsize=sent/(1024*1024*1024)
+
+                        progress["value"] = sent
+                        percentage_label.config(text=f"{percent:.2f}%")
+                        speed_label_Numeric.config(text=f"{speed:.2f} MB/s") 
+                        ETA_label_Numeric.config(text=f"{eta/60:.2f} Minutes")   
+                        sentSize_label.config(text=f"{sentsize:.2f} {sizetxt}")
                     window.update_idletasks()
     
             sock.close()
-            status_label.config(text=f"Sent: {filename} ({filesize} bytes)")
+            status_label.config(text=f"Sent: {filename} ({filesize} bytes){host}")
+            progress["value"] = filesize
+            percentage_label.config(text="100%")
+            speed_label_Numeric.config(text="") 
+            ETA_label_Numeric.config(text="")
+            sentSize_label.config(text=f"{sentsize} {sizetxt}")
             messagebox.showinfo("Success", f"File sent: {filename}",parent=window)
             Send_Btn.config(state="normal")
-            progress["value"] = 0
         except Exception as e:
             Send_Btn.config(state="normal")
             status_label.config(text="Error during send")
+            progress["value"] = sent
+            percentage_label.config(text=f"{percent:.2f}%")
+            speed_label_Numeric.config(text="") 
+            ETA_label_Numeric.config(text="")
             messagebox.showerror("Error", str(e),parent=window)    
     
     
@@ -103,10 +157,9 @@ def Send():
     window.iconphoto(True,image_icon)
     
     sendbackground=PhotoImage(file=r"Images/sender.png")
-    sendback=Label(window,image=sendbackground)
+    sendback=Label(window,image=sendbackground,bg="#ab95b5")
     sendback.place(x=10,y=15)
     
-    Frame(window,width=500,height=2,bg="#f3f5f6").place(x=0,y=220)
     
     #Lower Frame
     mainbackground=PhotoImage(file=r"Images/id.png")
@@ -121,17 +174,53 @@ def Send():
     Label(window,text="Selected Files:",font =('arial',10),bg="#ab95b5",fg="#070b12").place(x=160,y=60)
     Label(window, textvariable=file_var, width=33, anchor="w", relief="sunken").place(x=160,y=90)
     
-    #Enter IP
-    Label(window,text="Enter Receiver IP:",font =('arial',10),bg="#ab95b5",fg="#070b12").place(x=160,y=5)
+    #Enter Host ID
+    Label(window,text="Enter Receiver ID:",font =('arial',10),bg="#ab95b5",fg="#070b12").place(x=160,y=5)
     Entry(window, textvariable=IP_var, width=33).place(x=160,y=30)
+
+    #Percentage
+    percentage_label=Label(window, text=" ",font =('arial',12),bg="#ab95b5")
+    percentage_label.place(x=400,y=185)
+
+    #Speed
+    speed_label=Label(window, text="Speed",font =('arial',14,'bold'),bg="#ab95b5")
+    speed_label.place(x=20,y=225)
+    speed_label_Numeric=Label(window, text="",font =('arial',12),bg="#ab95b5")
+    speed_label_Numeric.place(x=10,y=250)
+
+    #ETA
+    ETA_label=Label(window, text="ETA",font =('arial',14,'bold'),bg="#ab95b5")
+    ETA_label.place(x=362,y=225)
+    ETA_label_Numeric=Label(window, text="",font =('arial',12),bg="#ab95b5")
+    ETA_label_Numeric.place(x=336,y=250)
+
+    ##Filesize
+    #Sent_label=Label(window, text="Sent",font =('arial',14,'bold'),bg="#ab95b5")
+    #Sent_label.place(x=195,y=225)
+    #Sent_numeric=Label(window, text="100 MB",font =('arial',11),bg="#ab95b5")
+    #Sent_numeric.place(x=176,y=250)
+
+    #Sent slash Totalsize
+    slash=Label(window,text="",font =('arial',15,'bold'),bg="#ab95b5")
+    slash.place(x=185,y=198)
+    totalSize_label=Label(window,text="",font =('arial',11),bg="#ab95b5")
+    totalSize_label.place(x=195,y=201)
+
+    sentSize_label=Label(window,text="",font =('arial',11),bg="#ab95b5")
+    sentSize_label.place(x=105,y=201)
+
+    #progress Bar
+    progress = Progressbar(window, length=382, mode="determinate")
+    progress.place(x=12,y=178)
+
+    # Separator Line
+    Frame(window,width=500,height=2,bg="#f3f5f6").place(x=0,y=225)
+
     
     #Status Label
     status_label = Label(window, text="Status: Idle",bg="#ab95b5")
-    status_label.place(x=15,y=240)
+    status_label.place(x=15,y=290)
     
-    #progress Bar
-    progress = Progressbar(window, length=420, mode="determinate")
-    progress.place(x=12,y=185)
     
     #buttons
     Button(window,text="+ Select File",command=select_file,width=10,height=1,font='arial 14 bold',bg="#fff",fg="#000").place(x=160,y=130)
